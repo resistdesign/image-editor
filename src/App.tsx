0b046b997175090c4b58137dd24796ba0d1273dc
Ryanne Graff <ryantgraff@resistdesign.com>
2023-11-24 13:18:55 -0500

    feat: (App) Add basic setup.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+
+export const App: FC = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-4xl">Hello, world!</h1>
+      <p className="text-lg">This is a paragraph.</p>
+    </div>
+  );
+};
